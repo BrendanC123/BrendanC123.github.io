ff8c0ac580364aba885bc1bdc2332b33f0400fcd
Brendan Collins <91950914+BrendanC123@users.noreply.github.com>
2021-12-02 10:24:47 +0000

    code

---
+++ assets/js/age.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                    + (currentdate.getMonth()+1)  + "/" 
+                    + currentdate.getFullYear() + " @ "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+    document.getElementById("age").innerHTML = datetime;
+}
