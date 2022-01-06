a232037ec9bbceb9a16529ccd8428d38da7e9fe3
Brendan Collins <91950914+BrendanC123@users.noreply.github.com>
2022-01-06 16:38:45 +0000

    program code

--- assets/js/age.js
+++ assets/js/age.js
@@ -1,10 +1,30 @@
-function myFunction(){
-    var currentdate = new Date(); 
-    var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                    + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear() + " @ "  
-                    + currentdate.getHours() + ":"  
-                    + currentdate.getMinutes() + ":" 
-                    + currentdate.getSeconds();
-    document.getElementById("age").innerHTML = datetime;
+function renderTime(){
+    // Date
+    var myDate = new Date();
+    var year = myDate.getFullYear();
+    if(year < 1000){
+        year += 1900
+    }
+    var day = myDate.getDay();
+    var month = myDate.getMonth();
+    var daym = myDate.getDate();
+    // Date End
+
+    //Time
+    var currentTime = newDate();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    //var milliseconds = currentTime.getMilliSeconds();
+    if(hours == 24){
+        hours = 0;
+    }
+    
+    var myClock = document.getElementById("clockDisplay");
+    myClock.textContent = "" + daym + " " + month + " " + year + " | " + hours + ":" + minutes + ":" + seconds;
+    myClock.innerText = "" + daym + " " + month + " " + year + " | " + hours + ":" + minutes + ":" + seconds;
+    
+    setTimeout("renderTime()", 1000)
+    //Time End
 }
+renderTime();
